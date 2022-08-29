e13fc704e7e88a845796a7a66905101fa6f1a6bc
benaiah-ke <benaiahpraise@gmail.com>
2022-08-29 07:56:46 +0300

    Done with lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Remove the <main>
+document.querySelector('main').remove();
+
+// Create h1#victory element
+var newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+
+// Set the text
+newHeader.innerHTML = 'YOUR-NAME is the champion';
